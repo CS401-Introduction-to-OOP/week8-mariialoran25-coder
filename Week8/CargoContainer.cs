62d69535b66e11993e1019fd6d61fd5c145cca1e
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-24 19:10:33 +0200

    . . .

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,23 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> _obj = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _obj.Add(item);
+        Console.WriteLine($"Added object {item.TrackingNumber}");
+    }
+    
+    public double GetTotalCost()
+    {
+        double res = 0;
+        foreach (var obj in _obj)
+        {
+            res += obj.CalculateCost();
+        }
+
+        return res;
+    }
+}
