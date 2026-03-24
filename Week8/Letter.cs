30043c0d136258fe66cbc7c25313bee240cd7f82
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-24 18:59:22 +0200

    . . .

---
+++ Week8/Letter.cs
@@ -0,0 +1,13 @@
+﻿namespace Week8;
+
+public class Letter :DeliveryItem
+{
+    public Letter(string trakingNumber, double weight) : base(trakingNumber, weight){}
+
+    public override double CalculateCost()
+    {
+        return 15 + (Weight * 10);
+    }
+
+    
+}
---
+++ Week8/Parcel.cs
@@ -0,0 +1,23 @@
+﻿namespace Week8;
+
+public class Parcel : DeliveryItem
+{
+    public string Dimensions { get; set; }
+
+    public Letter(string trakingNumber, double weight, string dimensions) : base(trakingNumber, weight)
+    {
+        Dimensions = dimensions;
+    }
+
+    public override double CalculateCost()
+    {
+        return 50 + (Weight * 25);
+    }
+
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($"Dimensions: {Dimensions}");
+
+    }
+}
